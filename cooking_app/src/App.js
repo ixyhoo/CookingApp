6f87ed5e1e0d5679a9f9cd06767d54dd40a53641
ixyhoo <ixyhoo@outlook.fr>
2023-09-05 11:03:50 +0200

    I ' done meals data

--- cooking_app/src/App.js
+++ cooking_app/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 const App = () => {
+
+  const [mealsData, setMealsData] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://www.themealdb.com/api/json/v1/1/search.php?s=chicken")
+    .then((res) => console.log(res.data.meals));
+  });
+
   return (
     <div className="app-container">
       <h1>My Cooking App</h1>
       <input type="text" placeholder='Type the name of a food' />
       <div className="meals-container">
-        
+
       </div>
     </div>
 
