4149a235a62f57160918433ca410e4db0b7c52ee
ixyhoo <ixyhoo@outlook.fr>
2023-09-05 14:31:00 +0200

    debug card page

--- cooking_app/src/App.js
+++ cooking_app/src/App.js
@@ -1,22 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Card from './components/Card';                                                                                             
 
 
 const App = () => {
 
   const [mealsData, setMealsData] = useState([]);
-
+  const [inputSearch, setInputSearch] = useState('');
   useEffect(() => {
     axios
-    .get("https://www.themealdb.com/api/json/v1/1/search.php?s=chicken")
+    .get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + inputSearch)
     .then((res) => setMealsData(res.data.meals));
-  });
+  }, [inputSearch]);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
 
   return (
     <div className="app-container">
       <h1>My Cooking App</h1>
-      <input type="text" placeholder='Type the name of a food' />
+      <input type="text" placeholder='Type the name of a food'
+      onChange={(e) => setInputSearch(e.target.value)} 
+      />
       <div className="meals-container">
+        {mealsData &&
+        mealsData
+        .slice(0, 24)
+        .map((meal) => <card key={meal.idMeal} meal={meal}/>)
+        }
 
       </div>
     </div>
@@ -24,4 +32,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default App;                 
